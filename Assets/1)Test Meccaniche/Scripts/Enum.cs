d4f49056b789d9f8f86827520b39c90ec18f903a
Matteo Veluti <Velutimatteo@gmail.com>
2023-03-14 14:35:35 +0100

    Add enum Script

---
+++ Assets/1)Test Meccaniche/Scripts/Enum.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheChroniclesOfEllen
+{
+    public enum Area
+    {
+        Ship,
+        Temple1,
+        Temple2,
+        ChomperNest
+    }
+
+    public enum Progression
+    {
+        Start,
+        TempleEntr,
+        Boss1Dead,
+        Boss2Dead,
+        KeyGet,
+        FinalBossDead,
+        End
+    }
+
+    public enum EnemyType
+    {
+        Chomper,
+        FastChomper,
+        Spitter,
+        Golem,
+        BigChomper,
+        GolemBoss,
+        GunGolem
+    }
+
+    public enum WeaponType
+    {
+        None,
+        Pistol,
+        Staff,
+        FinalWeapon
+    }
+}
