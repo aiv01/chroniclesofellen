7f5c7306643e74be11a6522894aa1b2fb38119df
Matteo Veluti <Velutimatteo@gmail.com>
2023-03-16 15:10:29 +0100

    Add save and load

---
+++ Assets/1)Test Meccaniche/Scripts/SafeFile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace TheChroniclesOfEllen
+{
+
+    [System.Serializable]
+    public class SafeFile
+    {
+        public int MaxHp;
+        public int DamageScale;
+        public bool HasDoubleJump;
+        public bool HasKey;
+        public int Progression;
+        public int Area;
+        public int SavePointNumber;
+
+        public override string ToString()
+        {
+            string s = "MaxHp: " + MaxHp +
+                "\nDamageScale: " + DamageScale + "" +
+                "\nJump: " + HasDoubleJump +
+                "\nKey: " + HasKey +
+                "\nProgression: " + Progression +
+                "\nArea: " + Area +
+                "\nsavepoint: " + SavePointNumber;
+            return s;
+        }
+    }
+}
