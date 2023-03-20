e2c4f91dc642e8f306557daa52933d5974071582
Samuele Macchi <macchisam@gmail.com>
2023-03-20 15:10:09 +0100

    Added following bullet

--- Assets/1)Test Meccaniche/Scripts/Weapons/Bullet.cs
+++ Assets/1)Test Meccaniche/Scripts/Weapons/Bullet.cs
@@ -11,17 +11,26 @@ namespace TheChroniclesOfEllen
         public int damage;
         public float speed;
         public Vector3 direction;
+        private Transform directionTarget;
         private float bulletTimer;
         private float bulletLifeTime = 3.5f;
 
         private void Start()
         {
             bulletTimer = 0;
+            directionTarget = GameObject.Find("ShootTarget").transform;
         }
 
         private void Update()
         {
-            transform.position += direction * speed * Time.deltaTime;
+            
+            if(directionTarget.parent != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position,directionTarget.position,speed * Time.deltaTime);
+            }else
+            {
+                transform.position += direction * speed * Time.deltaTime;
+            }
             bulletTimer += Time.deltaTime;
             if(bulletTimer >= bulletLifeTime)
             {
